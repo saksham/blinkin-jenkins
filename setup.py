0f2684fea0bf30195ab2b65c5e536021f60de4e1
Saksham Gautam <saksham.gautam@partner.wirecard.com>
2016-03-10 13:23:41 +0100

    Initial project layout.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='blinkin-jenkins',
+      version=version,
+      description="Connecting traffic light to CI Jenkins using Raspberry Pi",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='traffic-light jenkins raspberry-pi sketching electronics',
+      author='Saksham Gautam',
+      author_email='saksham@no-reply.github.com',
+      url='http://github.com/saksham/blinkin-jenkins',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
